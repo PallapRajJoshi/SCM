5a8b2fb99df1b0beebee6fab18a9142721fd6801
PallapRajJoshi <pallaprajjoshi@gmail.com>
2024-08-02 15:17:21 +0545

    button text is done if light thim dark and dart to light button text and all page dark and light mode

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -9,11 +9,13 @@ function changeTheme() {
     //set to web page
     document.querySelector("html").classList.add(currentTheme);
 
+
+    updateButtonText();
     //set the listener to change theme button
     const changeThemeButton = document.querySelector("#theme_change_button");
-    changeThemeButton.addEventListener("click", () => {
+    changeThemeButton.addEventListener("click", (event) => {
         console.log("changed button clicked");
-        const oldTheme=currentTheme;
+        const oldTheme = currentTheme;
         if (currentTheme === "dark") {
             //light theme
 
@@ -25,12 +27,32 @@ function changeTheme() {
         setTheme(currentTheme);
 
 
-//remove the old theme
-document.querySelector("html").classList.remove(oldTheme);
+        //remove the old theme
+        document.querySelector("html").classList.remove(oldTheme);
 
         //set the current theme
         document.querySelector("html").classList.add(currentTheme);
+
+        //updating the button text
+        updateButtonText();
+
     });
+}
+
+
+
+
+function updateButtonText() {
+
+
+    const changeThemeButton = document.querySelector("#theme_change_button");
+
+    // change the text of button
+
+    changeThemeButton.querySelector("span").textContent = currentTheme == "light" ? "Dark" : "Light";
+
+
+
 }
 
 // set theme to localstorage
