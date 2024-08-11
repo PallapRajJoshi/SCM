8ada8c9a433ffa3788b9ecdb3293d521d1f62aba
PallapRajJoshi <pallaprajjoshi@gmail.com>
2024-08-11 16:08:35 +0545

    new commit after long time

--- src/main/java/com/smart_content_manager/content_manager/model/User.java
+++ src/main/java/com/smart_content_manager/content_manager/model/User.java
@@ -16,11 +16,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+@Data
 @Entity
 
 @Builder
-@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "users")
